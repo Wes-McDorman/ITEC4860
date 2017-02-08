cc3ba13a91c3e14e4d03d69f81a20bc70210b4ff
Wes <wes16b@gmail.com>
2017-02-08 02:45:01 +0000

    Cleaned up code, changed variable names, set for loop boundaries

--- mainFolder/scripts/getProf.js
+++ mainFolder/scripts/getProf.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   var DOMLocation = 14; //start at "instructors"
-    for(i = 0; i < 10; i++) { //need to have a way to find out how many instructors were in "datadisplaytable"
+  var numElements = $(".datadisplaytable").children().children().children().length; //Number of elements
+
+
+    for(i = 0; i < numElements + 1; i++) { //need to have a way to find out how many instructors were in "datadisplaytable"
       //DOM navigation (look it up)
       $(".datadisplaytable").children().children().children().eq(DOMLocation).css("border", "10px solid red");
       DOMLocation += 17; //continue onto the next instructor box

ca689805c3d7dc28f4c88a92e35b2a3612473a93
dburkeggc <DBurkeGGC@gmail.com>
2017-02-09 19:56:08 +0000

    numElements fix, Ratings column added, restrict execution to specific page

--- mainFolder/scripts/getProf.js
+++ mainFolder/scripts/getProf.js
@@ -1,11 +1,32 @@
-$(document).ready(function(){
-  var DOMLocation = 14; //start at "instructors"
-  var numElements = $(".datadisplaytable").children().children().children().length; //Number of elements
+$(document).ready(
 
+function()
+{
+  //start at "Instructor"
+  var DOMLocation = 14;
+  
+  //whether or not "Instructor" is present in column headings
+  var instructorsPresent = $(".datadisplaytable").children().children().children().eq(DOMLocation).text().indexOf("Instructor") > -1;
+  
+  //number of instructor rows
+  var numElements = $(".datadisplaytable").children().children().length-2;
 
-    for(i = 0; i < numElements + 1; i++) { //need to have a way to find out how many instructors were in "datadisplaytable"
-      //DOM navigation (look it up)
-      $(".datadisplaytable").children().children().children().eq(DOMLocation).css("border", "10px solid red");
-      DOMLocation += 17; //continue onto the next instructor box
+  if(instructorsPresent)
+  {
+    //append "Rating" column heading after "Instructor" heading
+    $(".datadisplaytable").children().children().children().eq(DOMLocation).after("<th class='ddheader' scope='col'><abbr title='Rating'>Rating</abbr></th>");
+    DOMLocation += 18;
+    
+    //iterate through remaining elements based on number of instructors present
+    for(i = 0; i < numElements; i++)
+    { 
+      //append (specified text) after appropriate instructor cell
+      $(".datadisplaytable").children().children().children().eq(DOMLocation).after("<td class='dddefault'>RMPDATAGET</td>");
+      
+      //increment DOM element index to position of instructor cell of next row
+      DOMLocation += 18;
+    }
   }
-});
+}
+
+);

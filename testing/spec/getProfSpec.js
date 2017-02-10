f755848b583093ec765858daf192834f003e9774
FatThompson <matthewbiggiethompson@gmail.com>
2017-02-09 19:20:07 -0500

    adding basic jasmine stuff.

---
+++ testing/spec/getProfSpec.js
@@ -0,0 +1,11 @@
+describe("getProf", function(){
+
+  it("Testing Jasmine with true statements", function() {
+    expect(true).toEqual(true);
+  });
+
+  it("Testing Jasmine with false statement", function() {
+    expect(true).toEqual(!false);
+  });
+
+});

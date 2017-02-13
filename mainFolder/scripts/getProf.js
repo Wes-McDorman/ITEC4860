fca70dc51a0a38ac9a93af53bb0e76f9d9139e45
dburkeggc <DBurkeGGC@gmail.com>
2017-02-13 17:06:58 +0000

    setting up for server processing

--- mainFolder/scripts/getProf.js
+++ mainFolder/scripts/getProf.js
@@ -8,20 +8,42 @@ function()
   //whether or not "Instructor" is present in column headings
   var instructorsPresent = $(".datadisplaytable").children().children().children().eq(DOMLocation).text().indexOf("Instructor") > -1;
   
-  //number of instructor rows
-  var numElements = $(".datadisplaytable").children().children().length-2;
+  //number of instructor rows in table
+  var numRows = $(".datadisplaytable").children().children().length-2;
+  
+  //string replacement variable
+  var trimmedName = "";
+  //json text of instructor names
+  var instructors = [];
 
   if(instructorsPresent)
   {
+    //DOM location set to first instructor cell
+    DOMLocation = 31;
+    
+    for(i = 0; i < numRows; i++)
+    {
+      //split instructor cell text & push only instructor name to instructors array
+      trimmedName = $(".datadisplaytable").children().children().children().eq(DOMLocation).text().split(/[(]+/);
+      instructors.push( trimmedName[0] );
+      DOMLocation += 17;
+    }
+    
+    //send JSON.stringify(the array) as message for processing, to listener in background (sends actual request to server)
+    
+    
+    
+    //reset DOM location for data processing
+    DOMLocation = 14;
+    
     //append "Rating" column heading after "Instructor" heading
     $(".datadisplaytable").children().children().children().eq(DOMLocation).after("<th class='ddheader' scope='col'><abbr title='Rating'>Rating</abbr></th>");
     DOMLocation += 18;
-    
-    //iterate through remaining elements based on number of instructors present
-    for(i = 0; i < numElements; i++)
+
+    for(i = 0; i < numRows; i++)
     { 
       //append (specified text) after appropriate instructor cell
-      $(".datadisplaytable").children().children().children().eq(DOMLocation).after("<td class='dddefault'>RMPDATAGET</td>");
+      $(".datadisplaytable").children().children().children().eq(DOMLocation).after("<td class='dddefault'>"+instructors[i]+"</td>");
       
       //increment DOM element index to position of instructor cell of next row
       DOMLocation += 18;

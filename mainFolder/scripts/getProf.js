e1282bc5973fdb62532d25838c81843809f58af0
Dale Burke <dburke.ggc@gmail.com>
2017-02-27 16:12:34 -0500

    no

--- mainFolder/scripts/getProf.js
+++ mainFolder/scripts/getProf.js
@@ -2,15 +2,15 @@ $(document).ready(
 
 function()
 {
-  //start at "Instructor"
+  //start at "Instructors"
   var DOMLocation = 14;
-  
+
   //whether or not "Instructor" is present in column headings
   var instructorsPresent = $(".datadisplaytable").children().children().children().eq(DOMLocation).text().indexOf("Instructor") > -1;
-  
+
   //number of instructor rows in table
   var numRows = $(".datadisplaytable").children().children().length-2;
-  
+
   //string replacement variable
   var trimmedName = "";
   //json text of instructor names
@@ -20,7 +20,7 @@ function()
   {
     //DOM location set to first instructor cell
     DOMLocation = 31;
-    
+
     for(i = 0; i < numRows; i++)
     {
       //split instructor cell text & push only instructor name to instructors array
@@ -28,23 +28,23 @@ function()
       instructors.push( trimmedName[0] );
       DOMLocation += 17;
     }
-    
+
     //send JSON.stringify(the array) as message for processing, to listener in background (sends actual request to server)
-    
-    
-    
+
+
+
     //reset DOM location for data processing
     DOMLocation = 14;
-    
+
     //append "Rating" column heading after "Instructor" heading
     $(".datadisplaytable").children().children().children().eq(DOMLocation).after("<th class='ddheader' scope='col'><abbr title='Rating'>Rating</abbr></th>");
     DOMLocation += 18;
 
     for(i = 0; i < numRows; i++)
-    { 
+    {
       //append (specified text) after appropriate instructor cell
       $(".datadisplaytable").children().children().children().eq(DOMLocation).after("<td class='dddefault'>"+instructors[i]+"</td>");
-      
+
       //increment DOM element index to position of instructor cell of next row
       DOMLocation += 18;
     }
